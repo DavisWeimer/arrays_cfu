e00fbb5d441200533bea97ea41c0968c61ef797e
Davis Weimer <davisweimer@gmail.com>
2023-03-29 22:00:05 -0400

    Add 3 index positions

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,5 @@
 stringz = ["Chips", "Soda", "Candy", "Ice Cream"]
-numberz = [3, 360, 9, 120]
+integerz = [3, 360, 9, 120]
 floatz = [12.22, 13.33, 540.43, 25.23]
 booleanz = [true, false, false, true]
 
@@ -7,10 +7,22 @@ booleanz = [true, false, false, true]
 puts stringz.length
 
 # .pop will puts the last value of the array and remove it from the array
-puts numberz.pop
+puts integerz.pop
 
 # .shift will puts the first value of the array and remove it from the array
 puts floatz.shift
 
 # .push will add a value(s) to the array
 puts booleanz.push("false")
+
+# Index Positions below
+# Ruby doesn't start at 1, it starts at 0.
+
+puts stringz[0]
+# will puts Chips
+
+puts integerz[3]
+# will puts 120
+
+puts booleanz[2]
+# will puts false
