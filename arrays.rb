6c9b0b2e0934e05d69018b631a011f4b0aaad7a4
Davis Weimer <davisweimer@gmail.com>
2023-03-29 21:53:38 -0400

    Add 4 methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ stringz = ["Chips", "Soda", "Candy", "Ice Cream"]
 numberz = [3, 360, 9, 120]
 floatz = [12.22, 13.33, 540.43, 25.23]
 booleanz = [true, false, false, true]
+
+# .length will puts the number of values in the array
+puts stringz.length
+
+# .pop will puts the last value of the array and remove it from the array
+puts numberz.pop
+
+# .shift will puts the first value of the array and remove it from the array
+puts floatz.shift
+
+# .push will add a value(s) to the array
+puts booleanz.push("false")
